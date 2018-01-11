e8d3ed3b03caa21444a83e116ad4fe9f92254665
Michael Crawford <michael@Michaels-MacBook-Pro.local>
2018-01-11 17:28:20 -0500

    modified so that the google trends data is pulled using a Google Cloud Function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import SearchModal from "./components/SearchModal.js";
 import { Button, notification } from "antd";
-import * as googleTrends from "google-trends-api";
 import testData from "./data/testData.js";
 import Graph from "./components/Graph.js";
 import "./App.css";
@@ -11,8 +10,7 @@ class App extends Component {
     super(props);
     this.state = {
       searchParams: [],
-      isModalVisible: false,
-      searchData: []
+      isModalVisible: false
     };
   }
 
@@ -23,34 +21,10 @@ class App extends Component {
     });
   }
 
-  // grabs the relevant data from Google Trends about the selected currencies within
-  // the selected timeframe
-  searchGoogleTrends(keywords, timeframe, index) {
-    let startDate = new Date();
-    startDate.setDate(startDate.getDate() - timeframe);
-
-    let searchObject = {
-      keyword: keywords,
-      startTime: startDate,
-      granularTimeResolution: true
-    };
-
-    googleTrends
-      .interestOverTime(searchObject)
-      .then(results => {
-        let resultsObject = JSON.parse(results);
-        console.log(resultsObject);
-      })
-      .catch(error =>
-        notification.error({
-          message: "ERROR",
-          description: "There was an error processing your request."
-        })
-      );
-  }
-
   render() {
-    console.log(testData.default.timelineData);
+    const graphs = this.state.searchParams.map(param => {
+      return <Graph searchParams={param} />;
+    });
     return (
       <div className="App">
         <h1> CryptoPortal </h1>
@@ -63,13 +37,15 @@ class App extends Component {
           Add New Graph(s){" "}
         </Button>
 
+        <br />
+        <br />
+
         <SearchModal
           isVisible={this.state.isModalVisible}
           searchParams={this.state.searchParams}
           updateParent={(field, newValue) => this.updateField(field, newValue)}
         />
-
-        <Graph data={testData.default.timelineData} />
+        <div style={{ display: "flex", flexWrap: "wrap" }}>{graphs}</div>
       </div>
     );
   }
